0df79845f6c01b1ce5b061fe3f6a805db1ba4202
dojo2022-WatanabeTakaho <103238123+dojo2022-WatanabeTakaho@users.noreply.github.com>
2022-06-13 16:17:21 +0900

    Create Test.java

---
+++ src/src/test/Test.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");//この中のloginをファイル名に変えてください
+		dispatcher.forward(request, response);
+	}
+
+}
