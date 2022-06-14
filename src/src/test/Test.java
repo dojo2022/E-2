ed326cb1b93886bda5ec6a96eafde308ded506a8
dojo2022-koki-iida <103238200+dojo2022-koki-iida@users.noreply.github.com>
2022-06-14 17:56:22 +0900

    Update Test.java

--- src/src/test/Test.java
+++ src/src/test/Test.java
@@ -21,7 +21,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bmi.jsp");//この中のloginをファイル名に変えてください
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mypage.jsp");//この中のloginをファイル名に変えてください
 		dispatcher.forward(request, response);
 	}
 
