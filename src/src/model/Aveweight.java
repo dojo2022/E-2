4f36482427eceedd279fb2c51a2b2488adff7410
dojo2022-WatanabeTakaho <103238123+dojo2022-WatanabeTakaho@users.noreply.github.com>
2022-06-14 15:32:47 +0900

    set get作成

--- src/src/model/Aveweight.java
+++ src/src/model/Aveweight.java
@@ -1,5 +1,34 @@
 package model;
 
 public class Aveweight {
+private String gender;//性別
+private int age;//年齢
+private double aveweight;//平均体重
+public Aveweight() {
+}
+public Aveweight(String gender, int age, double aveweight) {
+	this.gender = gender;
+	this.age = age;
+	this.aveweight = aveweight;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public double getAveweight() {
+	return aveweight;
+}
+public void setAveweight(double aveweight) {
+	this.aveweight = aveweight;
+}
+
 
 }
