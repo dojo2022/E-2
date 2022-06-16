25fa471a0f07ba7c3c2958fe12f8cf1047c4a376
dojo-Watanabe <103239605+dojo-Watanabe@users.noreply.github.com>
2022-06-16 15:34:18 +0900

    修正

--- src/src/model/Meal.java
+++ src/src/model/Meal.java
@@ -1,45 +1,59 @@
 package model;
 
-public class Meal {
-	private String userid;
-	private int foodnumber;
-	private int daily;
-	private String meal;
-	private int satiety;
-	public Meal(String userid, int foodnumber, int daily, String meal, int satiety) {
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Meal implements Serializable{
+	private String userid; //userid
+	private int foodnumber; //食事番号
+	private Date daily;// 日付
+	private String meal;//食事写真
+	private int satiety;//満腹度
+
+	public Meal(String userid, int foodnumber, Date daily, String meal, int satiety) {
 		this.userid = userid;
 		this.foodnumber = foodnumber;
 		this.daily = daily;
 		this.meal = meal;
 		this.satiety = satiety;
 	}
+
 	public String getUserid() {
 		return userid;
 	}
+
 	public void setUserid(String userid) {
 		this.userid = userid;
 	}
+
 	public int getFoodnumber() {
 		return foodnumber;
 	}
+
 	public void setFoodnumber(int foodnumber) {
 		this.foodnumber = foodnumber;
 	}
-	public int getDaily() {
+
+	public Date getDaily() {
 		return daily;
 	}
-	public void setDaily(int daily) {
+
+	public void setDaily(Date daily) {
 		this.daily = daily;
 	}
+
 	public String getMeal() {
 		return meal;
 	}
+
 	public void setMeal(String meal) {
 		this.meal = meal;
 	}
+
 	public int getSatiety() {
 		return satiety;
 	}
+
 	public void setSatiety(int satiety) {
 		this.satiety = satiety;
 	}
