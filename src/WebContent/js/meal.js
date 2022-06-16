559feb58cd66196b2275e0a2721aeb684671ec0b
dojo2022-hamahata <103239387+dojo2022-hamahata@users.noreply.github.com>
2022-06-16 16:03:11 +0900

    aaa

--- src/WebContent/js/meal.js
+++ src/WebContent/js/meal.js
@@ -1,14 +1,5 @@
 /*
 *
-let elems = document.getElementsByName("num");
-let val = "";
-
-for (let i = 0; i < elems.length; i++) {
-	if (elems[i].checked){
-		val = elems[i].value;
-	}
-	document.getElementById("result").textContent = val;
-}
 
 *
  */
--- src/src/servlet/MealServlet.java
+++ src/src/servlet/MealServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  * Servlet implementation class MealServlet
@@ -19,7 +20,8 @@ public class MealServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/meal.jsp");//この中のmealをファイル名に変えてください
 		dispatcher.forward(request, response);
@@ -28,9 +30,32 @@ public class MealServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		Part part = request.getPart("IMAGE"); // getPartで取得
+
+		String image = this.getFileName(part);
+		request.setAttribute("image", image);
+		// サーバの指定のファイルパスへファイルを保存
+		//場所はクラス名↑の上に指定してある
+		part.write(image);
+		//ディスパッチ
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/meal.jsp");
+		dispatcher.forward(request, response);
 	}
 
+	//ファイルの名前を取得してくる
+	private String getFileName(Part part) {
+		String name = null;
+		for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+			if (dispotion.trim().startsWith("filename")) {
+				name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+				name = name.substring(name.lastIndexOf("\\") + 1);
+				break;
+			}
+		} // TODO 自動生成されたメソッド・スタブ
+		return name;
+	}
 }
