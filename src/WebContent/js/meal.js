fba8c366b87987458d6f331043f9129cfc51bb0a
dojo2022-hamahata <103239387+dojo2022-hamahata@users.noreply.github.com>
2022-06-16 16:04:18 +0900

    Revert "aaa"
    This reverts commit 559feb58cd66196b2275e0a2721aeb684671ec0b.

--- src/WebContent/js/meal.js
+++ src/WebContent/js/meal.js
@@ -1,5 +1,14 @@
 /*
 *
+let elems = document.getElementsByName("num");
+let val = "";
+
+for (let i = 0; i < elems.length; i++) {
+	if (elems[i].checked){
+		val = elems[i].value;
+	}
+	document.getElementById("result").textContent = val;
+}
 
 *
  */
--- src/src/model/Meal.java
+++ src/src/model/Meal.java
@@ -6,7 +6,6 @@ public class Meal {
 	private int daily;
 	private String meal;
 	private int satiety;
-
 	public Meal(String userid, int foodnumber, int daily, String meal, int satiety) {
 		this.userid = userid;
 		this.foodnumber = foodnumber;
@@ -14,43 +13,33 @@ public class Meal {
 		this.meal = meal;
 		this.satiety = satiety;
 	}
-
 	public String getUserid() {
 		return userid;
 	}
-
 	public void setUserid(String userid) {
 		this.userid = userid;
 	}
-
 	public int getFoodnumber() {
 		return foodnumber;
 	}
-
 	public void setFoodnumber(int foodnumber) {
 		this.foodnumber = foodnumber;
 	}
-
 	public int getDaily() {
 		return daily;
 	}
-
 	public void setDaily(int daily) {
 		this.daily = daily;
 	}
-
 	public String getMeal() {
 		return meal;
 	}
-
 	public void setMeal(String meal) {
 		this.meal = meal;
 	}
-
 	public int getSatiety() {
 		return satiety;
 	}
-
 	public void setSatiety(int satiety) {
 		this.satiety = satiety;
 	}
