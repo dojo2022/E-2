3d26edda271eaa5ea6e0597d36aca4bc37082148
dojo-Watanabe <103239605+dojo-Watanabe@users.noreply.github.com>
2022-06-17 16:08:20 +0900

    signup

--- src/src/dao/UserDao.java
+++ src/src/dao/UserDao.java
@@ -1,9 +1,16 @@
 package dao;
 
+import model.Contact;
+
 public class UserDao {
 	//dailyを参照
 	public int finddaily() {
 		return 0;
 
+	}
+
+	public boolean contactin(Contact contact) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
 	}
 }
--- src/src/servlet/SignupServlet.java
+++ src/src/servlet/SignupServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDao;
+import model.Contact;
+import model.Result;
 
 /**
  * Servlet implementation class SignupServlet
@@ -30,7 +35,34 @@ public class SignupServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
+		request.setCharacterEncoding("UTF-8");
+		String userid = request.getParameter("ID");
+		String email = request.getParameter("EMAIL");
+        String contact= request.getParameter("TEXT");
+        String loginid =request. getParameter("lOGINID") ;
+        String loginpassword = request .getParameter ("PW");
+        String gender = request .getParameter("GENDER");
+        String dateofbirth
+        String targetweight
+        String registration
+
+        UserDao cDao = new UserDao();
+		if (cDao.contactin(new Contact (userid, email,contact))){		// 新規登録画面成功
+			// セッションスコープにIDを格納する
+			HttpSession session = request.getSession();
+			session.getAttribute("");
+						response.sendRedirect("/healthcare/LoginServlet");
+		}
+
+		else {					// 新規登録失敗
+				request.setAttribute("result",
+				new Result("！", ", ", "/healthcare/LoginServlet"));
+
+									// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+				dispatcher.forward(request, response);
+			}
+	}
 }
+
