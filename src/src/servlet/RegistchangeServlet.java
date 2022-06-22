f252fff3db9226f32715272e65e875b03ddc321d
dojo2022-koki-iida <103238200+dojo2022-koki-iida@users.noreply.github.com>
2022-06-22 13:21:46 +0900

    Update RegistchangeServlet.java

--- src/src/servlet/RegistchangeServlet.java
+++ src/src/servlet/RegistchangeServlet.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.Userdata;
+
+
 /**
  * Servlet implementation class RegistchangeServlet
  */
@@ -25,10 +28,14 @@ public class RegistchangeServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 
 		HttpSession session = request.getSession();
+
 		if (session.getAttribute("Userid") == null) {
 			response.sendRedirect("/healthcare/LoginServlet");
 			return;
 		}
+
+		Userdata userid = (Userdata) session.getAttribute("userid");
+
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/registchange.jsp");//この中のloginをファイル名に変えてください
 		dispatcher.forward(request, response);
@@ -40,7 +47,7 @@ public class RegistchangeServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+
 	}
 
 }
