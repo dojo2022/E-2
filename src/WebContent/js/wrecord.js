3dc30b746d11a9951d5c15771c95f302721f5fc0
dojo2022-nakamura <103239324+dojo2022-nakamura@users.noreply.github.com>
2022-06-27 17:53:27 +0900

    体重記録のjs

--- src/WebContent/js/wrecord.js
+++ src/WebContent/js/wrecord.js
@@ -1,14 +1,17 @@
 let formObj = document.getElementById('form');
-  let errorMessageObj = document.getElementById('error_message');
+let errorMessageObj = document.getElementById('error_message');
 
-  formObj.onsubmit = function(event) {
-    if (formObj.id.value === "" ) {
-      errorMessageObj.textContent = '※体重を入力してください';
-      return false;
-     }
-	 if (formObj.id.value === "" ) {
-      errorMessageObj.textContent = '※数値を入力してください';
-      return false;
-     }
-     errorMessageObj.textContent = null;
-  };
+
+formObj.onsubmit = function(event) {
+	if (formObj.id.value === "") {
+		errorMessageObj.textContent = '※体重を入力してください';
+		return false;
+	}
+	else if (parseInt(id)) {
+		errorMessageObj.textContent = null;
+
+	} else {
+		errorMessageObj.textContent = '※数値を入力してください';
+		return false;
+	}
+}
